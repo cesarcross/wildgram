7ba2b29088e3b8b94c35e4fc1e2cf3992a6d324c
Cesar Corregiari <cesarcorregiari@gmail.com>
2020-08-24 17:46:10 -0300

    done

--- src/components/Title.js
+++ src/components/Title.js
@@ -4,8 +4,8 @@ const Title = () => {
   return (
     <div className="title">
       <h1>WildGram</h1>
-      <h2>Your Pictures</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <h2>Gallery</h2>
+      <p>Wildlife and landscape pictures</p>
     </div>
   );
 };
